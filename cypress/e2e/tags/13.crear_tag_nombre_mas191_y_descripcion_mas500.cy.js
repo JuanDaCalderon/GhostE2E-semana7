c2a574c54f44f2edfd292a1cabcdea8494d5a272
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2023-05-19 17:02:57 -0500

    Se crean los escenarios de tags

---
+++ cypress/e2e/tags/13.crear_tag_nombre_mas191_y_descripcion_mas500.cy.js
@@ -0,0 +1,42 @@
+import configJson from '../../../config/config.json';
+import loginPage from "../../page-object/login";
+import TagsTag from "../../page-object/tags";
+import { faker } from '@faker-js/faker';
+
+const nameScreenshots = 'tags/crear_tag/crear_tag_nombre_mas191_descripcion_mas500';
+
+const tagData = {
+    name: faker.random.alpha(192),
+    description: faker.random.alpha(501)
+}
+
+describe('Escenarios tags', () => {
+    it('Crear tag con nombre y descripción validas', () => {
+        let i = 0
+
+        // Given
+        cy.visit(configJson.host);
+
+        // When
+        loginPage.login(configJson.user, configJson.password);
+        cy.screenshot(`${nameScreenshots}${i += 1}`, { overwrite: true });
+
+        TagsTag.goToTagButtonMain();
+
+        TagsTag.goToNewTagButton();
+
+        TagsTag.enterTagName(tagData.name);
+
+        TagsTag.enterTagDescription(tagData.description);
+
+        TagsTag.goToSaveTag();
+
+        TagsTag.goToTagButtonTags();
+
+        TagsTag.goToleaveTags();
+
+        // Then
+        cy.get("h3.gh-tag-list-name").should('not.exist');
+        cy.screenshot(`${nameScreenshots}${i += 1}`, { overwrite: true });
+    })
+})
---
+++ cypress/e2e/tags/16.crear_tag_nombre_caracter_especial_y_numero.cy.js
@@ -0,0 +1,45 @@
+import configJson from '../../../config/config.json';
+import loginPage from "../../page-object/login";
+import TagsTag from "../../page-object/tags";
+import { faker } from '@faker-js/faker';
+
+const nameScreenshots = 'tags/crear_tag/crear_tag_nombre_charespecial_y_numero';
+
+const chars = ['@', '#', '$', '%', '&', '!'];
+const specialChar = chars[Math.floor(Math.random() * chars.length)];
+const tagData = {
+    name: faker.random.alphaNumeric(5) + specialChar + specialChar,
+    description: faker.random.alphaNumeric(10)
+};
+
+describe('Escenarios tags', () => {
+    it('Crear tag con nombre y descripción validas', () => {
+        let i = 0
+
+        // Given
+        cy.visit(configJson.host);
+
+        // When
+        loginPage.login(configJson.user, configJson.password);
+        cy.screenshot(`${nameScreenshots}${i += 1}`, { overwrite: true });
+
+        TagsTag.goToTagButtonMain();
+
+        TagsTag.goToNewTagButton();
+
+        TagsTag.enterTagName(tagData.name);
+
+        TagsTag.enterTagDescription(tagData.description);
+
+        TagsTag.goToSaveTag();
+
+        TagsTag.goToTagButtonTags();
+
+        // Then
+        cy.get("h3.gh-tag-list-name").contains(tagData.name).should('exist');
+        cy.screenshot(`${nameScreenshots}${i += 1}`, { overwrite: true });
+
+        // Clean the enviroment tested
+        TagsTag.cleanRecentTag(cy.get("h3.gh-tag-list-name").contains(tagData.name));
+    })
+})
