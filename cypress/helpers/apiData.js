67a6afb3fb46d65306ad0fc5a93afac312df7aba
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2023-05-19 19:38:32 -0500

    Api data entity updated

--- cypress/helpers/apiData.js
+++ cypress/helpers/apiData.js
@@ -1,62 +1,51 @@
 class ApiDataPool {
-    async getRandomIndex(max) {
-        return Math.floor(Math.random() * max);
-    }
 
     async getRandomFirstName() {
         const response = await fetch("https://my.api.mockaroo.com/data-pool.json?key=7d257dd0");
         const dataPool = await response.json();
-        const index = await this.getRandomIndex(10);
-        return dataPool[index].first_name;
+        return dataPool.first_name;
     }
 
     async getRandomLastName() {
         const response = await fetch("https://my.api.mockaroo.com/data-pool.json?key=7d257dd0");
         const dataPool = await response.json();
-        const index = await this.getRandomIndex(10);
-        return dataPool[index].last_name;
+        return dataPool.last_name;
     }
 
     async getRandomEmail() {
         const response = await fetch("https://my.api.mockaroo.com/data-pool.json?key=7d257dd0");
         const dataPool = await response.json();
-        const index = await this.getRandomIndex(10);
-        return dataPool[index].email;
+        return dataPool.email;
     }
 
     async getRandomPassword() {
         const response = await fetch("https://my.api.mockaroo.com/data-pool.json?key=7d257dd0");
         const dataPool = await response.json();
-        const index = await this.getRandomIndex(10);
-        return dataPool[index].password;
+        return dataPool.password;
     }
 
     async getRandomShortSentence() {
         const response = await fetch("https://my.api.mockaroo.com/data-pool.json?key=7d257dd0");
         const dataPool = await response.json();
-        const index = await this.getRandomIndex(10);
-        return dataPool[index].short_sentence;
+        return dataPool.short_sentence;
     }
 
     async getRandomLongSentence() {
         const response = await fetch("https://my.api.mockaroo.com/data-pool.json?key=7d257dd0");
         const dataPool = await response.json();
-        const index = await this.getRandomIndex(10);
-        return dataPool[index].long_sentence;
+        return dataPool.long_sentence;
     }
 
     async getRandomNumbers() {
         const response = await fetch("https://my.api.mockaroo.com/data-pool.json?key=7d257dd0");
         const dataPool = await response.json();
-        const index = await this.getRandomIndex(10);
-        return dataPool[index].numbers;
+        return dataPool.numbers;
     }
 
     async getRandomNaughty() {
         const response = await fetch("https://my.api.mockaroo.com/data-pool.json?key=7d257dd0");
         const dataPool = await response.json();
-        const index = await this.getRandomIndex(10);
-        return dataPool[index].Naughty;
+        return dataPool.Naughty;
     }
 }
 
